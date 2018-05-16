4e9c548609b5037a9b365ee9763ab8fd3819d9f8
Teresa Li <teresa@Teresas-MacBook-Pro.local>
2018-05-16 12:58:30 -0400

    first push

--- game/index.js
+++ game/index.js
@@ -1,6 +1,27 @@
 import {Map} from 'immutable'
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+let board = Map()
+const MOVE = 'MOVE'
+
+const move1 = function(turn, [row, col]) {
+  console.log('in moveCreator')
+  return {type: MOVE, position: [row, col], turn: turn}
 }
+
+
+export default function gameReducer(state={turn:'X', board}, action) {
+  switch(action.type) {
+    case MOVE:
+      state.board.setIn(action.position, state.turn)
+      if (state.turn === 'X') {
+        console.log('in switch', state.turn)
+        return {turn: 'Y', board}
+      } else {
+        return {turn: 'X', board}
+      }
+    default:
+      return state
+  }
+}
+
+export {move1}
